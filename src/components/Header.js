192a849e7ce509ad086d246b6970a309020e79dc
Yash Shamnani <shyamnaniyash@gmail.com>
2023-05-02 12:15:47 +0530

    header cart dropdown

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,37 +1,85 @@
-import React from 'react'
-import {Navbar,Container, FormControl,Nav,Dropdown, Badge} from "react-bootstrap"
-import {FaShoppingCart} from "react-icons/fa"
-import { Link } from 'react-router-dom'
+import React from "react";
+import {
+  Navbar,
+  Container,
+  FormControl,
+  Nav,
+  Dropdown,
+  Badge,Button
+} from "react-bootstrap";
+import { FaShoppingCart } from "react-icons/fa";
+import { AiFillDelete } from "react-icons/ai";
+import { Link } from "react-router-dom";
+import { CartState } from "../context/Context";
 const Header = () => {
+  const {
+    state: { cart },
+    dispatch,
+  } = CartState();
   return (
-    <div> 
-        <Navbar bg="dark" variant="dark" style={{height:80}}>
-            <Container>
-                <Navbar.Brand>
-                    <Link href='/'>Shopping Cart</Link>
-                </Navbar.Brand>
-                <Navbar.Text className='search'>
-                    <FormControl style={{width:500}} placeholder="Search for product" className='m-auto'>
+    <div>
+      <Navbar bg="dark" variant="dark" style={{ height: 80 }}>
+        <Container>
+          <Navbar.Brand>
+            <Link to="/">Shopping Cart</Link>
+          </Navbar.Brand>
+          <Navbar.Text className="search">
+            <FormControl
+              style={{ width: 500 }}
+              placeholder="Search for product"
+              className="m-auto"
+            ></FormControl>
+          </Navbar.Text>
+          <Nav>
+            <Dropdown alignRight>
+              <Dropdown.Toggle variant="success" id="dropdown-basic">
+                <FaShoppingCart color="white" fontSize="25px" />
+                <Badge>{cart.length}</Badge>
+              </Dropdown.Toggle>
 
-                    </FormControl>
-                </Navbar.Text>
-                <Nav>
-                <Dropdown alignRight>
-                      <Dropdown.Toggle variant="success" id="dropdown-basic">
-                      <FaShoppingCart color="white" fontSize="25px" />
-                           <Badge>{10}</Badge>
-                        </Dropdown.Toggle>
-
-                        <Dropdown.Menu style={{minWidth:370}}>
-                          <span style={{padding:10}}>Cart is Empty!</span>
-                      </Dropdown.Menu>
-                </Dropdown>
-                </Nav>
-            </Container>
-
-        </Navbar>
+              <Dropdown.Menu style={{ minWidth: 370 }}>
+                {cart.length > 0 ? (
+                  <>
+                    {cart.map((prod) => (
+                      <span className="cartitem" key={prod.id}>
+                        <img
+                          src={prod.image}
+                          className="cartItemImg"
+                          alt={prod.name}
+                        />
+                        <div className="cartItemDetail">
+                          <span>{prod.name}</span>
+                          <span>₹ {prod.price.split(".")[0]}</span>
+                        </div>
+                        <AiFillDelete
+                          fontSize="20px"
+                          style={{ cursor: "pointer" }}
+                          onClick={() =>
+                            dispatch({
+                              type: "REMOVE_CART",
+                              payload: prod,
+                            })
+                          }
+                        />
+                       
+                      </span>
+                    ))}
+                      <Link to='/cart'>
+                          <Button style={{ width: "95%", margin: "0 10px" }}>
+                      Go To Cart
+                    </Button>
+                        </Link>
+                  </>
+                ) : (
+                  <span style={{ padding: 10 }}>Cart is Empty!</span>
+                )}
+              </Dropdown.Menu>
+            </Dropdown>
+          </Nav>
+        </Container>
+      </Navbar>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
